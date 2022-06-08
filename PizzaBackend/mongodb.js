0b885115f34470c4d0462c3f6046ac6a0e8fac51
Jay Shankar Sharma <jayshankar.s@wisflux.com>
2022-06-08 14:58:47 +0530

    Start and edn docker compose in workflow update 2

--- PizzaBackend/mongodb.js
+++ PizzaBackend/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/pizzaapp',
+mongoose.connect(process.env.DB_URL ||'mongodb://127.0.0.1:27017/pizzaapp',
 (err)=>{
     if(!err){
 console.log("DB connection successfull");
